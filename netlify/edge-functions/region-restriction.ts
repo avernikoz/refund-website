1fb34fb6a5563bc89690c9f780569fbf7c80ab19
avernikoz <avernikoz@gmail.com>
2024-04-04 22:12:06 +0300

    upd restricted country codes list: remove colombia

--- netlify/edge-functions/region-restriction.ts
+++ netlify/edge-functions/region-restriction.ts
@@ -1,6 +1,6 @@
 import { Context } from "@netlify/edge-functions";
 
-export const RESTRICTED_COUNTRY_CODES_LIST = ["CU", "KP", "IR", "SY", "CO"];
+export const RESTRICTED_COUNTRY_CODES_LIST = ["CU", "KP", "IR", "SY"];
 
 export const RESTRICTED_REGIONS_LIST = ["Crimea", "Donetsk", "Luhansk"];
 
